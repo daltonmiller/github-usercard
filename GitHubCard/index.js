1854a438512f2fa30313e29904eedd2d82aaad3b
Dalton Miller <daltonmiller1145@Daltons-MacBook-Pro.local>
2020-07-09 19:53:23 -0600

    finished

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,7 +3,19 @@
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+///import axios from 'axios'
 
+
+const cardsAttach = document.querySelector('.cards');
+
+axios.get('https://api.github.com/users/daltonmiller')
+.then(response => {
+  const gitInfo = response.data;
+  cardsAttach.appendChild(createUserCard(gitInfo));
+})
+.catch(error => {
+  console.log(error);
+})
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -16,6 +28,64 @@
   STEP 4: Pass the data received from Github into your function,
     and append the returned markup to the DOM as a child of .cards
 */
+function createUserCard(obj){
+
+  // Create DOM elements
+
+  const card = document.createElement('div');
+  const userImg = document.createElement('img');
+  const cardInfo = document.createElement('div');
+  const name = document.createElement('h3');
+  const userName = document.createElement('p');
+  const location = document.createElement('p');
+  const profile = document.createElement('p');
+  const profileAdd = document.createElement('a');
+  const followers = document.createElement('p');
+  const following = document.createElement('p');
+  const bio = document.createElement('p');
+
+
+  // Set classes
+
+  card.className = ('card');
+  cardInfo.className = ('card-info');
+  name.className = ('name');
+  userName.className = ('username');
+
+
+  // Set contents / text
+
+  userImg.setAttribute('src', obj.avatar_url);
+  name.textContent = obj.name;
+  userName.textContent = obj.login;
+  location.textContent = obj.location;
+  profile.textContent = 'Profile: '
+  profileAdd.textContent = obj.html_url;
+  //profileAdd.textContent = `${obj.html_url}`;
+  profileAdd.setAttribute('href', obj.html_url);
+  followers.textContent = `Followers: ${obj.followers}`;
+  following.textContent = `Following: ${obj.following}`;
+  bio.textContent = `Bio: ${obj.bio}`;
+
+
+  // Create Structure
+
+  card.appendChild(userImg);
+  card.appendChild(cardInfo);
+  cardInfo.appendChild(name);
+  cardInfo.appendChild(userName);
+  cardInfo.appendChild(location);
+  cardInfo.appendChild(profile);
+  profile.appendChild(profileAdd);
+  cardInfo.appendChild(followers);
+  cardInfo.appendChild(following);
+  cardInfo.appendChild(bio);
+
+
+  return card;
+
+
+}
 
 /*
   STEP 5: Now that you have your own card getting added to the DOM, either
@@ -27,9 +97,24 @@
     Using that array, iterate over it, requesting data for each user, creating a new card for each
     user, and adding that card to the DOM.
 */
+const followersArray = ['tetondan', 'dustinmyers', 'justsml', 'luishrd', 'bigknell'];
 
-const followersArray = [];
-
+followersArray.map(name => {
+  axios.get(`https://api.github.com/users/${name}`)
+    .then(response => {
+      const gitInfo = response.data;
+      cardsAttach.appendChild(createUserCard(gitInfo));
+    })
+    .catch(error => {
+      console.log(error) 
+    })
+})
+axios.get(`https://lambda-times-backend.herokuapp.com/topics`).then(function (something) {
+  console.log('dito', something)
+}).catch(function (error) {
+  console.log('dito', error)
+})
+console.log('here')
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
@@ -50,11 +135,9 @@ const followersArray = [];
     </div>
 */
 
-/*
-  List of LS Instructors Github username's:
-    tetondan
-    dustinmyers
-    justsml
-    luishrd
-    bigknell
-*/
+
+
+
+
+
+
